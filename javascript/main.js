4e443bac9ad420e4536b69ad861cc0d766e93607
matheusfcunha <matheusfbcunha@gmail.com>
2017-03-03 15:47:51 -0300

    Fixed divGrid, added mouse hover first effect

--- javascript/main.js
+++ javascript/main.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
 
   var divCount = 0;
 
-    while (divCount < 257) {
+    while (divCount < 256) {
     divCount++
     $('#wrapper').append("<div></div>")
     $('#wrapper > div').addClass("divGrid");
   };
 
+  $('.divGrid').mouseenter(function(){
+
+     $(this).css("background-color", "yellow");
+
+  });
 
 });
