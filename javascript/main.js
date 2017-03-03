badcd4d2cf44859a47a32548327936514df4636f
matheusfcunha <matheusfbcunha@gmail.com>
2017-03-03 19:49:01 -0300

    Added  color scheme

--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var divCount = 0;
 
+    var divCount = 0;
     while (divCount < 256) {
-    divCount++;
-    $('#wrapper').append("<div></div>");
-  };
-
-  $('#wrapper > div').addClass("divGrid");
-
-  $('.divGrid').mouseenter(function(){
-     $(this).css("background-color", "yellow");
-  });
-
-
-  $('#newSketch').click(function (){
-
-    $('#wrapper > div').remove();
-    var height = 500;
-    var width = 800;
-      var userNumber = prompt('How many pixels do you want? Example: 32 (32x32)');
-      var userPixels = parseInt(userNumber);
-
-
-      var divCount = userPixels;
-      var loopCount = userPixels * userPixels;
-
-        while (divCount < loopCount + userPixels) {
         divCount++;
         $('#wrapper').append("<div></div>");
-      };
+    };
 
-       $('#wrapper > div').addClass("divGrid");
+    $('#wrapper > div').addClass("divGrid");
+
+/*----------------------COLOR CHANGE-------------------------------*/
 
 
-    formattedHeight = height / userPixels;
-    formattedWidth = width / userPixels;
-
-    $('.divGrid').height(formattedHeight);
-    $('.divGrid').width(formattedWidth);
-
-    $('.divGrid').mouseenter(function(){
-
-       $(this).css("background-color", "yellow");
-
+   $("#bg").change(function(){
+        $(".divGrid").css("background-color",$("#bg").val());
     });
 
-  });
+
+/*----------------------------------------------------------------*/
+
+    $('.divGrid').mouseenter(function() {
+        $(this).css("background-color",$("#color").val());
+    });
+
+
+    $('#newSketch').click(function() {
+
+        $('#wrapper > div').remove();
+        var height = 500;
+        var width = 800;
+        var userNumber = prompt('How many pixels do you want? Example: 32 (32x32)');
+        var userPixels = parseInt(userNumber);
+
+
+        var divCount = userPixels;
+        var loopCount = userPixels * userPixels;
+
+        while (divCount < loopCount + userPixels) {
+            divCount++;
+            $('#wrapper').append("<div></div>");
+        };
+
+        $('#wrapper > div').addClass("divGrid");
+
+        formattedHeight = height / userPixels;
+        formattedWidth = width / userPixels;
+        $('.divGrid').height(formattedHeight);
+        $('.divGrid').width(formattedWidth);
+
+
+    $('.divGrid').mouseenter(function() {
+        $(this).css("background-color",$("#color").val());
+    });
+
+    });
 
 });
