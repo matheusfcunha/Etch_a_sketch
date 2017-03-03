f99eefd5d9bc4295d56c4efada01ecf0a6625597
matheusfcunha <matheusfbcunha@gmail.com>
2017-03-03 18:33:14 -0300

    Finished almost everything, just needs to stylize the page

--- javascript/main.js
+++ javascript/main.js
@@ -3,20 +3,48 @@ $(document).ready(function(){
   var divCount = 0;
 
     while (divCount < 256) {
-    divCount++
-    $('#wrapper').append("<div></div>")
-    $('#wrapper > div').addClass("divGrid");
+    divCount++;
+    $('#wrapper').append("<div></div>");
   };
 
+  $('#wrapper > div').addClass("divGrid");
+
   $('.divGrid').mouseenter(function(){
-
      $(this).css("background-color", "yellow");
-
   });
+
 
   $('#newSketch').click(function (){
 
-    alert('hello!');
+    $('#wrapper > div').remove();
+    var height = 500;
+    var width = 800;
+      var userNumber = prompt('How many pixels do you want? Example: 32 (32x32)');
+      var userPixels = parseInt(userNumber);
+
+
+      var divCount = userPixels;
+      var loopCount = userPixels * userPixels;
+
+        while (divCount < loopCount + userPixels) {
+        divCount++;
+        $('#wrapper').append("<div></div>");
+      };
+
+       $('#wrapper > div').addClass("divGrid");
+
+
+    formattedHeight = height / userPixels;
+    formattedWidth = width / userPixels;
+
+    $('.divGrid').height(formattedHeight);
+    $('.divGrid').width(formattedWidth);
+
+    $('.divGrid').mouseenter(function(){
+
+       $(this).css("background-color", "yellow");
+
+    });
 
   });
 
